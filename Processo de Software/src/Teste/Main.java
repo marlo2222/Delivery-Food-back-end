e716bd64e112a1077a45de681beb464fbd71be07
marlo2222 <40809563+marlo2222@users.noreply.github.com>
2018-08-09 19:24:11 -0300

    Update Main.java

--- Processo de Software/src/Teste/Main.java
+++ Processo de Software/src/Teste/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("hello wold");
-		https://github.com/marlo2222/Processo-de-software.git
+		print("pull");
 	}
 
 }
