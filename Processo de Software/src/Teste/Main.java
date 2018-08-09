9dfecca6cdd498c9eb3a45747d2718467b5128b1
marlo <marlo@LAPTOP-AR893P8D>
2018-08-09 19:39:03 -0300

    mudando

--- Processo de Software/src/Teste/Main.java
+++ Processo de Software/src/Teste/Main.java
@@ -4,12 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("hello wold");
-<<<<<<< HEAD
+
 		System.out.println("deu certo nao");
-		
-=======
-		print("pull");
->>>>>>> branch 'master' of https://github.com/marlo2222/Processo-de-software.git
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(i);
+		}
+
+
 	}
 
 }
