9a90cc1e2b1c68d0145e3c28ed8d3f399c8f78f5
marlo <marlo@10.5.223.15>
2018-08-10 08:52:05 -0300

    mudei

--- Processo de Software/src/Teste/Main.java
+++ Processo de Software/src/Teste/Main.java
@@ -5,17 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("hello wold");
 
-		System.out.println("deu certo nao");
-		for (int i = 0; i < args.length; i++) {
-			System.out.println(i);
-			System.out.println(2);
-
-			System.out.println(i+1+1+2);
-			System.out.println(i+1+1+1);
-			
-		}
-
-
 	}
 
 }
