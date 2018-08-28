0c7278e23543ba4d1b5c24f29425f80895e9f80b
marlo2222 <marloh2222@gmail.com>
2018-08-27 21:53:59 -0300

    Criando o atributos do proprietario

---
+++ Processo de Software/src/Negocios/Proprietario.java
@@ -0,0 +1,14 @@
+package Negocios;
+
+import java.util.ArrayList;
+
+public class Proprietario {
+	private String nomeProprietario;
+	private String emailProprietario;
+	private int cpfProprietario;
+	private int contatoProprietario;
+	private int rgProprietario;
+	protected int Senha;
+	private ArrayList<Restaurantes> restaurante;
+
+}
