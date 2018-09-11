383c07fed784b264a0aae3b3d182c4c65849e0c1
marlo2222 <marloh2222@gmail.com>
2018-09-11 08:15:09 -0300

    adicionando classe usuario

--- Processo de Software/src/Negocios/Usuario.java
+++ Processo de Software/src/Negocios/Usuario.java
@@ -1,5 +1,50 @@
 package Negocios;
 
 public class Usuario {
+	
+	private String nome;
+	private String email;
+	private String cpf;
+	private String contato;
+	private int Senha;
 
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getContato() {
+		return contato;
+	}
+
+	public void setContato(String contato) {
+		this.contato = contato;
+	}
+
+	public int getSenha() {
+		return Senha;
+	}
+
+	public void setSenha(int senha) {
+		Senha = senha;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 }
